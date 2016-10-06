9c10dafc9e17fad56168998d1dba0a3aae081efc
GDYendell <gary.yendell@diamond.ac.uk>
2016-10-06 10:54:04 +0100

    Move GND, FBK and Cas data to config file

--- excaliburcalibrationdawn/excaliburnode.py
+++ excaliburcalibrationdawn/excaliburnode.py
@@ -2174,8 +2174,8 @@ class ExcaliburNode(object):
         by new modules, these DACs need to be re-adjusted. If modules are
         swapped the DACs have to be swapped in the corresponding array. For
         example GND_DAC is an array of size 6 (fems) x 8 (chips)
-        GND_DAC[x,:] will contain the GND DAC value of the 8 chips connected to
-        fem/node x+1 where x=0 corresponds to the top half of the top module
+        GND_DAC[x, :] will contain the GND DAC value of the 8 chips connected
+        to fem/node x+1 where x=0 corresponds to the top half of the top module
 
         [NUMBERING STARTS AT 0 IN PYTHON SCRIPTS WHEREAS NODES NUMBERING STARTS
         AT 1 IN EPICS]
@@ -2187,25 +2187,25 @@ class ExcaliburNode(object):
 
         The procedure to adjust a DAC manually is as follows:
         For GND dac of chip 0:
-        x.set_dac([0],'GND',150)
-        x.read_dac([0],'GND')
+        x.set_dac([0], 'GND', 150)
+        x.read_dac([0], 'GND')
         Once the DAC value correspond to the required analogue value, edit the
         GND_Dacs matrix:
-        GND_Dacs[0,:]=[NEW DAC VALUE,x,x,x,x,x,x,x]
+        GND_Dacs[0, :]=[NEW DAC VALUE, x, x, x, x, x, x, x]
 
         For FBK dac of chip 0:
-        x.set_dac([0],'FBK',190)
-        x.read_dac([0],'FBK')
+        x.set_dac([0], 'FBK', 190)
+        x.read_dac([0], 'FBK')
         Once the DAC value correspond to the required analogue value, edit the
         FBK_Dacs matrix:
-        FBK_Dacs[0,:]=[NEW DAC VALUE,x,x,x,x,x,x,x]
+        FBK_Dacs[0, :]=[NEW DAC VALUE, x, x, x, x, x, x, x]
 
         For Cas dac of chip 0:
-        x.set_dac([0],'Cas',180)
-        x.read_dac([0],'Cas')
+        x.set_dac([0], 'Cas', 180)
+        x.read_dac([0], 'Cas')
         Once the DAC value correspond to the required analogue value, edit the
         CAS_Dacs matrix:
-        CAS_Dacs[0,:]=[NEW DAC VALUE,x,x,x,x,x,x,x]
+        CAS_Dacs[0, :]=[NEW DAC VALUE, x, x, x, x, x, x, x]
 
         This process could be automated if many modules have to be calibrated.
 
@@ -2214,94 +2214,11 @@ class ExcaliburNode(object):
             fem: FEM to set
 
         """
-        GND_Dacs = 145*np.ones([6, 8]).astype('int')
-        FBK_Dacs = 190*np.ones([6, 8]).astype('int')
-        CAS_Dacs = 180*np.ones([6, 8]).astype('int')
-
-        # TOP MODULE: AC-EXC-8
-
-        gnd = 145
-        fbk = 190
-        cas = 180
-
-        # #@ Moly temp: 35 degC on node 3
-
-        GND_Dacs[0, :] = [141, 144, 154, 143, 161, 158, 144, 136]
-        FBK_Dacs[0, :] = [190, 195, 201, 198, 220, 218, 198, 192]
-        CAS_Dacs[0, :] = [178, 195, 196, 182, 213, 201, 199, 186]
-
-        # GND_Dacs[1,:]=[145,141,142,142,141,141,143,150]
-        # FBK_Dacs[1,:]=[205,190,197,200,190,190,200,210]
-        # CAS_Dacs[1,:]=[187,187,183,187,177,181,189,194]
-        GND_Dacs[1, :] = [154, 155, 147, 147, 147, 155, 158, 151]
-        # Max current for fbk limited to 0.589 for chip 7
-        FBK_Dacs[1, :] = [215, 202, 208, 200, 198, 211, 255, 209]
-        CAS_Dacs[1, :] = [208, 197, 198, 194, 192, 207, 199, 188]
-
-        # NOTE : chip 2 FBK cannot be set to target value
-
-        # CENTRAL MODULE: AC-EXC-7
-
-        # @ Moly temp: 27 degC on node 1
-        GND_Dacs[2, :] = [143, 156, 139, 150, 144, 150, 149, 158]
-        FBK_Dacs[2, :] = [189, 213, 185, 193, 204, 207, 198, 220]
-        CAS_Dacs[2, :] = [181, 201, 177, 184, 194, 193, 193, 210]
-
-        # @ Moly temp: 28 degC on node 2
-        GND_Dacs[3, :] = [143, 156, 139, 150, 144, 150, 149, 158]
-        FBK_Dacs[3, :] = [189, 213, 185, 193, 204, 207, 198, 220]
-        CAS_Dacs[3, :] = [181, 201, 177, 184, 194, 193, 193, 210]
-
-        # BOTTOM MODULE: AC-EXC-4
-
-        # #@ Moly temp: 31 degC on node 5
-        # GND_Dacs[4,:]=[136,146,136,160,142,135,140,148]
-        # FBK_Dacs[4,:]=[190,201,189,207,189,189,191,208]
-        # CAS_Dacs[4,:]=[180,188,180,197,175,172,185,200]
-
-        # @ Moly temp: 31 degC on node 6
-
-        # NOTE: DAC out read-back does not work for chip 2 (counting from 0) of
-        # bottom 1/2 module
-        # Using read_dac function on chip 2 will give FEM errors and the system
-        # will need to be power-cycled
-        # Got error on load pixel config command for chip 7: 2 Pixel
-        # configuration loading failed
-        # Exception caught during femCmd: Timeout on pixel configuration write
-        # to chip7 acqState=3
-        # Connecting to FEM at IP address 192.168.0.101 port 6969 ...
-        # **************************************************************
-        # Connecting to FEM at address 192.168.0.101
-        # Configuring 10GigE data interface: host IP: 10.0.2.1 port: 61649 FEM
-        # data IP: 10.0.2.2 port: 8 MAC: 62:00:00:00:00:01
-        # Acquisition state at startup is 3 sending stop to reset
-        # **** Loading pixel configuration ****
-        # Last idx: 65536
-        # Last idx: 65536
-        # Last id
-
-        gnd = 145
-        fbk = 190
-        cas = 180
-        # GND_Dacs[5,:]=[158,140,gnd,145,158,145,138,153]
-        # FBK_Dacs[5,:]=[215,190,fbk,205,221,196,196,210]
-        # CAS_Dacs[5,:]=[205,178,cas,190,205,180,189,202]
-
-        # 1st MODULE 1M:
-
-        # up -fem5
-        GND_Dacs[4, :] = [151, 135, 150, 162, 153, 144, 134, 145]
-        FBK_Dacs[4, :] = [200, 195, 205, 218, 202, 194, 185, 197]
-        CAS_Dacs[4, :] = [196, 180, 202, 214, 197, 193, 186, 187]
-        # Bottom -fem6
-        GND_Dacs[5, :] = [134, 145, 171, 146, 152, 142, 141, 141]
-        FBK_Dacs[5, :] = [192, 203, 228, 197, 206, 191, 206, 189]
-        CAS_Dacs[5, :] = [178, 191, 218, 184, 192, 186, 195, 185]
-
+        # TODO: Can we just call set_dac on chips?
         for chip in chips:
-            self.set_dac(range(chip, chip + 1), 'GND', GND_Dacs[fem - 1, chip])
-            self.set_dac(range(chip, chip + 1), 'FBK', FBK_Dacs[fem - 1, chip])
-            self.set_dac(range(chip, chip + 1), 'Cas', CAS_Dacs[fem - 1, chip])
+            self.set_dac([chip], 'GND', MPX3RX.GND_DAC[fem - 1, chip])
+            self.set_dac([chip], 'FBK', MPX3RX.FBK_DAC[fem - 1, chip])
+            self.set_dac([chip], 'Cas', MPX3RX.CAS_DAC[fem - 1, chip])
 
         # self.read_dac(range(8), 'GND')
         # self.read_dac(range(8), 'FBK')
