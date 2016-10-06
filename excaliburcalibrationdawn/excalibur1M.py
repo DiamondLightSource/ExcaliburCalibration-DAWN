6230f3f81f79cf22ff17d5ac3276bd2072485f01
GDYendell <gary.yendell@diamond.ac.uk>
2016-10-06 15:11:47 +0100

    Add some API functions to Excalibur1M

--- excaliburcalibrationdawn/excalibur1M.py
+++ excaliburcalibrationdawn/excalibur1M.py
@@ -1,20 +1,89 @@
 """A 1M Excalibur detector."""
-from collections import namedtuple
-
 from excaliburcalibrationdawn.excaliburnode import ExcaliburNode
+from excaliburcalibrationdawn.excaliburdawn import ExcaliburDAWN
+from excaliburcalibrationdawn import arrayutil as util
 
-Nodes = namedtuple("Nodes", "Node1 Node2")
+import numpy as np
 
 
 class Excalibur1M(object):
 
     """A class representing a 1M Excalibur detector composed of two nodes."""
 
+    node_shape = [256, 8*256]
+
     def __init__(self):
         """Initialise two ExcaliburNode instances as a 1M detector."""
-        self.nodes = Nodes(ExcaliburNode(0), ExcaliburNode(1))
+        self.nodes = [ExcaliburNode(1), ExcaliburNode(2)]
 
-    def optimize_dac_disc(self):
-        """"""
-        self.nodes.Node1.optimize_dac_disc()
-        self.nodes.Node2.optimize_dac_disc()
+        self.dawn = ExcaliburDAWN()
+
+    def threshold_equalization(self, chips):
+        """Calibrate discriminator equalization for given chips in detector.
+
+        Args:
+            chips: List of lists of chips for each node
+
+        """
+        for node_idx, node in enumerate(self.nodes):
+            node.threshold_equalization(chips[node_idx])
+
+    def optimize_dac_disc(self, chips, roi):
+        """Optimize discriminators for given chips in detector.
+
+        Args:
+            chips: List of lists of chips for each node
+            roi: Mask to apply during optimization
+
+        """
+        for node_idx, node in enumerate(self.nodes):
+            node_roi = self._grab_node_slice(roi, node_idx)
+            node.optimize_dac_disc(chips[node_idx], "DACDiscL", node_roi)
+            node.optimize_dac_disc(chips[node_idx], "DACDiscH", node_roi)
+
+    def expose(self, exposure_time):
+        """Acquire single image using.
+
+        Args:
+            exposure_time: Acquire time for image
+
+        Returns:
+            numpy.array: Image data
+
+        """
+        for node in self.nodes:
+            node.settings['acquire_time'] = exposure_time
+
+        image = self.nodes[0].expose()
+        for node_idx, node in enumerate(self.nodes[1:]):
+            image = np.concatenate((image, node.expose()), axis=1)
+
+        self.dawn.plot_image(image, "Excalibur1M Image")
+
+    def _grab_node_slice(self, array, node_idx):
+        """Grab a node from a full array.
+
+        Args:
+            array: Array to grab from
+            node_idx: Index of section of array to grab
+
+        Returns:
+            numpy.array: Sub array
+
+        """
+        start, stop = self._generate_node_range(node_idx)
+        return util.grab_slice(array, start, stop)
+
+    def _generate_node_range(self, node_idx):
+        """Calculate start and stop coordinates of given node.
+
+        Args:
+            node_idx: Chip to calculate range for
+
+        """
+        height = self.node_shape[0]
+        width = self.node_shape[1]
+
+        start = [node_idx * height, 0]
+        stop = [start[0] + height - 1, width - 1]
+        return start, stop
--- tests/excalibur1M_test.py
+++ tests/excalibur1M_test.py
@@ -5,9 +5,11 @@ require("mock")
 from mock import patch, MagicMock, ANY
 
 from excaliburcalibrationdawn.excalibur1M import Excalibur1M
-from excaliburcalibrationdawn.excaliburnode import ExcaliburNode
+from excaliburcalibrationdawn.excaliburnode import ExcaliburNode, np
 E1M_patch_path = "excaliburcalibrationdawn.excalibur1M.Excalibur1M"
-EN_patch_path = "excaliburcalibrationdawn.excalibur1M.ExcaliburNode"
+Node_patch_path = "excaliburcalibrationdawn.excalibur1M.ExcaliburNode"
+DAWN_patch_path = "excaliburcalibrationdawn.excalibur1M.ExcaliburDAWN"
+util_patch_path = "excaliburcalibrationdawn.arrayutil"
 
 
 class InitTest(unittest.TestCase):
@@ -16,8 +18,75 @@ class InitTest(unittest.TestCase):
         self.e = Excalibur1M()
 
     def test_class_attributes_set(self):
-        self.assertIsInstance(self.e.nodes.Node1, ExcaliburNode)
-        self.assertIsInstance(self.e.nodes.Node2, ExcaliburNode)
+        self.assertIsInstance(self.e.nodes[0], ExcaliburNode)
+        self.assertIsInstance(self.e.nodes[1], ExcaliburNode)
 
-        self.assertEqual(0, self.e.nodes.Node1.fem)
-        self.assertEqual(1, self.e.nodes.Node2.fem)
+        self.assertEqual(1, self.e.nodes[0].fem)
+        self.assertEqual(2, self.e.nodes[1].fem)
+
+
+class FunctionsTest(unittest.TestCase):
+
+    def setUp(self):
+        self.e = Excalibur1M()
+        self.node1_mock = MagicMock()
+        self.node2_mock = MagicMock()
+        self.e.nodes = [self.node1_mock, self.node2_mock]
+
+    def test_threshold_equalization(self):
+        self.e.threshold_equalization([[0], [0]])
+
+        self.node1_mock.threshold_equalization.assert_called_once_with([0])
+        self.node2_mock.threshold_equalization.assert_called_once_with([0])
+
+    @patch(E1M_patch_path + '._grab_node_slice')
+    def test_optimize_dac_disc(self, grab_mock):
+        roi_mock = MagicMock()
+        self.e.optimize_dac_disc([[0], [0]], roi_mock)
+
+        self.assertEqual((roi_mock, 0), grab_mock.call_args_list[0][0])
+        self.assertEqual((roi_mock, 1), grab_mock.call_args_list[1][0])
+        self.assertEqual(([0], "DACDiscL", grab_mock.return_value), self.node1_mock.optimize_dac_disc.call_args_list[0][0])
+        self.assertEqual(([0], "DACDiscH", grab_mock.return_value), self.node1_mock.optimize_dac_disc.call_args_list[1][0])
+        self.assertEqual(([0], "DACDiscL", grab_mock.return_value), self.node2_mock.optimize_dac_disc.call_args_list[0][0])
+        self.assertEqual(([0], "DACDiscH", grab_mock.return_value), self.node2_mock.optimize_dac_disc.call_args_list[1][0])
+
+    @patch(DAWN_patch_path + '.plot_image')
+    def test_expose(self, plot_mock):
+        mock_array = np.array([[0, 1],
+                               [1, 0]])
+        self.node1_mock.expose.return_value = mock_array
+        self.node2_mock.expose.return_value = mock_array
+        expected_array = np.array([[0, 1, 0, 1],
+                                   [1, 0, 1, 0]])
+
+        self.e.expose(100)
+
+        self.node1_mock.expose.assert_called_once_with()
+        self.node2_mock.expose.assert_called_once_with()
+        plot_mock.assert_called_once_with(ANY, "Excalibur1M Image")
+        np.testing.assert_array_equal(expected_array, plot_mock.call_args[0][0])
+
+
+class UtilTest(unittest.TestCase):
+
+    def setUp(self):
+        self.e = Excalibur1M()
+
+    @patch(util_patch_path + '.grab_slice')
+    @patch(E1M_patch_path + '._generate_node_range', return_value=["start", "stop"])
+    def test_grab_node_slice(self, generate_mock, grab_mock):
+        mock_array = MagicMock()
+        self.e._grab_node_slice(mock_array, 1)
+
+        generate_mock.assert_called_once_with(1)
+        grab_mock.assert_called_once_with(mock_array, "start", "stop")
+
+    def test_generate_node_range(self):
+        expected_start = [256, 0]
+        expected_stop = [511, 2047]
+
+        start, stop = self.e._generate_node_range(1)
+
+        self.assertEqual(expected_start, start)
+        self.assertEqual(expected_stop, stop)
